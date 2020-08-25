bb4e461f21882483f44f564128301c90ac685f0e
Zoran <zoransasko@inbox.com>
2020-08-26 00:04:58 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/push_notification_flutter_sample/Application.java
@@ -0,0 +1,25 @@
+package com.example.push_notification_flutter_sample;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FlutterFirebaseMessagingService.setPluginRegistrant(this);
+  }
+
+  @Override
+  public void registerWith(PluginRegistry registry) {
+//   GeneratedPluginRegistrant.registerWith(registry);
+  //
+    //
+    FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+  }
+}
